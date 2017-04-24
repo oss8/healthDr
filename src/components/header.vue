3356aa648e7da02fa898e8cca8d0652717223731
朱衢林 <0xjoker@bombvote.com>
2017-04-24 22:37:15 +0800

    init

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <div class="logo"></div>
+        <div class="login">您好<span class="userName">{{name}}</span><el-button type="text" @click="exit()">退出</el-button></div>
+    </div>
+</template>
+
+
+<style lang="scss">
+     @import "../style/common.scss";
+</style>
+<script>
+    export  default {
+        replace: true,
+        props: {name:String},
+        data () {
+            return {}
+        },
+        methods:{
+            exit() {
+                localStorage.removeItem('eshine_channel');
+                this.$router.replace({
+                                name: 'login'
+                            })
+            }
+        }
+    }
+  
+</script>
---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+    baseUrl:"http://opapi.eshine.cn:4500/api/"
+    // baseUrl:"http://localhost:4500/api/"
+} 
