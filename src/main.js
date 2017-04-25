363e18a9f0231cec75418ce8bf99cc76477b4e70
朱衢林 <0xjoker@bombvote.com>
2017-04-25 16:59:47 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import 'element-ui/lib/theme-default/index.css'
+
 import 'nprogress/nprogress.css'
+import '../theme/index.css'
+
 import VueRouter from 'vue-router';
 import App from './App'
 import router from './router'
@@ -11,7 +13,6 @@ import ElementUI from 'element-ui'
 import axios from 'axios';
 // import VueAxios from 'vue-axios';
 import './style/common.scss'
-
 Vue.use(ElementUI)
 Vue.use(VueRouter);
 // Vue.use(VueAxios, axios)
